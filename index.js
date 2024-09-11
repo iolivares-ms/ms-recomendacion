56a722f407ac2cdf239b10990f8484acf7e77416
iolivares-ms <iolivares@mercantilseguros.com>
2024-09-11 09:46:25 -0400

    page structure

---
+++ index.js
@@ -0,0 +1,31 @@
+ptypes = [{name:"SI | Panamá",pp:50},
+    {name:"SI | Classic",pp:40},
+    {name:"SI | Premium",pp:30},
+    {name:"SI | Access",pp:20},
+    {name:"SI | Afinidad",pp:10},
+    {name:"SI | Urgencias Médicas",pp:5},
+    {name:"SC | Colectivo",pp:20},
+    {name:"AU | Perdida Total",pp:30},
+    {name:"AU | Cobertura Amplia",pp:20},
+    {name:"AU | Rcv",pp:10},
+    {name:"MA | Ap",pp:20},
+    {name:"MA | Vida",pp:30},
+    {name:"MA | Funerario",pp:10},
+    {name:"MA | Pago Unico Cancer",pp:10},
+    {name:"MA | Renta",pp:30},
+    {name:"PA | Combinado",pp:50},
+    {name:"PA | Incendio",pp:20},
+    {name:"PA | Responsabilidad",pp:10},
+    {name:"PA | Todo Riesgo",pp:50},
+    {name:"PA | Riesgos Diversos",pp:30},
+    {name:"PA | Fianza",pp:20},
+    {name:"PA | Pyme",pp:10},
+    {name:"PA | Otros",pp:10},
+    ]
+
+
+function hello(){
+    console.log("Hola mundo")
+}
+
+hello()
